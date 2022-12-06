6e834c1007b1d9ca78ae5de77cf20073bb9c88c7
rudynakodach <107307636+rudynakodach@users.noreply.github.com>
2022-12-06 19:42:14 +0100

    Initial commit

---
+++ Client.cs
@@ -0,0 +1,103 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient
+{
+    class Client
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 100; //server is running on port 100
+
+        static void Main()
+        {
+            Console.Title = "Client";
+            InitialConnectLoop();
+            RequestLoop();
+            Exit();
+        }
+        /// <summary>
+        /// Attempt to connect to the server.
+        /// </summary>
+        private static void InitialConnectLoop()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Connection attempt " + attempts);
+                    ClientSocket.Connect(IPAddress.Loopback, PORT);
+                }
+                catch (SocketException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message + "\n"); //print the exception out to the console
+                    Console.ResetColor();
+                }
+            }
+
+            Console.WriteLine("\nConnected.\n"); //connection succeeded
+        }
+        /// <summary>
+        /// Loop receiving requests and keep the program alive.
+        /// </summary>
+        private static void RequestLoop()
+        {
+            while (true)
+            {
+                SendRequest();
+                ReceiveResponse();
+            }
+        }
+
+        /// <summary>
+        /// Close socket and exit program.
+        /// </summary>
+        private static void Exit()
+        {
+            SendString("exit"); 
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0); //exit code 0 - OK
+        }
+
+        private static void SendRequest()
+        {
+            Console.Write(">");
+            string request = Console.ReadLine();
+
+            SendString(request);
+            
+
+            if (request.ToLower() == "exit")
+            {
+                Exit();
+            }
+        }
+
+        /// <summary>
+        /// Send an encoded ASCII string to the server.
+        /// </summary>
+        private static void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ HostServer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+	class Host
+	{
+		public static Random r = new();
+
+		private static List<Socket> _clientSockets = new();
+
+		public static Socket _socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+		private static byte[] _buffer = new byte[1024];
+
+		public static void Main()
+		{
+			Console.Title = $"Server {_socketServer.AddressFamily}";
+
+			SetupServer();
+			Console.ReadLine();
+		}
+
+		public static void SetupServer()
+		{
+			Console.WriteLine("Setting up the server...");
+			_socketServer.Bind(new IPEndPoint(IPAddress.Any, 100));
+			_socketServer.Listen(5);
+
+
+			_socketServer.BeginAccept(new AsyncCallback(AcceptCallback), null);
+		}
+
+		public static void AcceptCallback(IAsyncResult AR)
+		{
+			Socket socket = _socketServer.EndAccept(AR);
+
+			_clientSockets.Add(socket);
+			Console.WriteLine("Client connected.");
+
+			socket.BeginReceive(_buffer, 0, _buffer.Length,SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+
+			_socketServer.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+		}
+
+		private static void ReceiveCallback(IAsyncResult AR)
+		{
+			Console.WriteLine("Request received!");
+			Socket socket = (Socket)AR.AsyncState;
+			int received = socket.EndReceive(AR);
+
+			byte[] dataBuf = new byte[received];
+			Array.Copy(_buffer, dataBuf, received);
+
+			string textReceived = Encoding.ASCII.GetString(dataBuf);
+			Console.WriteLine($"Text received: {textReceived}");
+
+			string response = string.Empty;
+
+			switch (textReceived.ToLower())
+			{
+				case string s when
+					s.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).Length == 3 &&
+					s.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries)[0] == "rng":
+
+					int min = int.Parse(s.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
+                    int max = int.Parse(s.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries)[2]);
+
+					response = $"Your generated number was {r.Next(min,max)}";
+                    break;
+
+				case string s when s.StartsWith("get time"):
+					response = DateTime.Now.ToLongDateString();
+					break;
+
+				default:
+					response = "Invalid request.";
+					break;
+			}
+
+			byte[] data = Encoding.ASCII.GetBytes(response);
+			socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+			_socketServer.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+		}
+
+		private static void SendCallback(IAsyncResult AR)
+		{
+			Console.WriteLine("Sending a callback...");
+			Socket socket = (Socket)AR.AsyncState;
+			socket.EndSend(AR);
+		}
+	}
+}
